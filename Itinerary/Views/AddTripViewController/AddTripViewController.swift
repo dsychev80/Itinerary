c1bab425acb01dcae2d5dacc297ee0a2f4acbc7b
Sychev Denis <dsychev80@icloud.com>
2020-12-05 17:43:05 +1200

    Add textField validation in AddTripViewController.

--- Itinerary/Views/AddTripViewController/AddTripViewController.swift
+++ Itinerary/Views/AddTripViewController/AddTripViewController.swift
@@ -29,11 +29,32 @@ class AddTripViewController: UIViewController {
     }
     
     @IBAction func save(_ sender: UIButton) {
-        guard let title = textField.text, !title.isEmpty else { return }
+        textField.rightViewMode = .never
+        
+        guard let title = textField.text, !title.isEmpty, title != "" else {
+            self.setupTextFieldValidation()
+            return
+        }
         TripFunctions.createTrip(tripModel: TripModel(title: title))
         guard let doneSaving = doneSaving else { return }
         doneSaving()
         dismiss(animated: true, completion: nil)
     }
     
+    private func setupTextFieldValidation() {
+        let imageView = UIImageView(frame: CGRect(x: 5, y: 5, width: 20, height: 20))
+        imageView.image = UIImage(named: "warning.png")
+        imageView.contentMode = .scaleAspectFit
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        view.addSubview(imageView)
+        
+        textField.rightView = view
+        textField.rightViewMode = .always
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.red.cgColor
+        textField.layer.cornerRadius = 5
+        
+        textField.placeholder = "Trip name required!"
+    }
 }
