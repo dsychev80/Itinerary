1edcdcd5ca88fafaf58698e306c416ee8c3ffdc7
Sychev Denis <dsychev80@icloud.com>
2020-12-08 16:10:55 +1200

    ActivitiesViewController added

---
+++ Itinerary/Views/ActivitiesViewController/ActivitiesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ActivitiesViewController.swift
+//  Itinerary
+//
+//  Created by Denis Sychev on 12/8/20.
+//
+
+import UIKit
+
+class ActivitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+    }
+}
